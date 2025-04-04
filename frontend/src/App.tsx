58161623a0363d7b14937bd12db710331d9557b8
SamanwoySaha <samanwoy.web@gmail.com>
2025-04-04 10:15:09 +0600

    added navbar, loginpage

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from './components/layout/Navbar'
+import Login from './components/layout/Login'
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+    </div>
+  )
+}
+
+export default App
