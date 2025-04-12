fc496233e3547f6160f4c2b97dd74fa018106503
SamanwoySaha <samanwoy.web@gmail.com>
2025-04-12 22:34:02 +0600

    added db

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,7 +2,12 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   prefix: "ui-",
   presets: [sharedConfig],
 };
